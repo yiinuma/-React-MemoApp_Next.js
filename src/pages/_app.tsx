4252b94b2b0ae5d6f3ada93e5d5eed02adf08630
yiinuma <y.iinuma211@gmail.com>
2022-04-16 23:42:02 +0900

    react-router-dom　削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
